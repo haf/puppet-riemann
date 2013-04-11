cf6b12964180b2cc93e5f03ecdf7ce8b2e3760b7
Henrik <henrik@haf.se>
2013-04-11 15:59:02 +0200

    depend on riemann::common

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,9 +1,13 @@
 class riemann::common(
   $group = $riemann::params::group
 ) inherits riemann::params {
+  anchor { 'riemann::common::start': }
+
   group { $group:
     ensure  => present,
     system  => true,
+    require => Anchor['riemann::common::start'],
+    before  => Anchor['riemann::common::end'],
   }
 
   ensure_packages($riemann::params::tools_packages)
@@ -12,7 +16,11 @@ class riemann::common(
     ensure   => 'installed',
     provider => gem,
     require  => [
-      Package[$riemann::params::tools_packages]
+      Package[$riemann::params::tools_packages],
+      Anchor['riemann::common::start']
     ],
+    before   => Anchor['riemann::common::end'],
   }
+  
+  anchor { 'riemann::common::end': }
 }
